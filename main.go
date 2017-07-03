edce583efeb1a44f2ed7096c89eaaa377ef3ef0e
vAlAi <valai@valaiMac.local>
2017-07-03 10:52:05 +0900

    make server

--- main.go
+++ main.go
@@ -1,5 +1,14 @@
 package main
 
+import "net/http"
+
 func main() {
-	println("Hello, World!")
+//	println("Hello, World!")
+
+	http.HandleFunc("/", hello)
+	http.ListenAndServe(":8080", nil)
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("hello!"))
 }
